1182875ef32592f70c80510f6374bd128867a13e
Clark Van Oyen <clark@countable.ca>
2016-10-30 13:39:45 -0700

    initial

---
+++ controls.js
@@ -0,0 +1,107 @@
+
+var controlsEnabled = false;
+var moveForward = false;
+var moveBackward = false;
+var moveLeft = false;
+var moveRight = false;
+var canJump = false;
+var prevTime = performance.now();
+var velocity = new THREE.Vector3();
+
+var blocker = document.getElementById( 'blocker' );
+var instructions = document.getElementById( 'instructions' );
+var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
+if ( havePointerLock ) {
+  var element = document.body;
+  var pointerlockchange = function ( event ) {
+    if ( document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element ) {
+      controlsEnabled = true;
+      controls.enabled = true;
+      blocker.style.display = 'none';
+    } else {
+      controls.enabled = false;
+      blocker.style.display = '-webkit-box';
+      blocker.style.display = '-moz-box';
+      blocker.style.display = 'box';
+      instructions.style.display = '';
+    }
+  };
+  var pointerlockerror = function ( event ) {
+    instructions.style.display = '';
+  };
+  // Hook pointer lock state change events
+  document.addEventListener( 'pointerlockchange', pointerlockchange, false );
+  document.addEventListener( 'mozpointerlockchange', pointerlockchange, false );
+  document.addEventListener( 'webkitpointerlockchange', pointerlockchange, false );
+  document.addEventListener( 'pointerlockerror', pointerlockerror, false );
+  document.addEventListener( 'mozpointerlockerror', pointerlockerror, false );
+  document.addEventListener( 'webkitpointerlockerror', pointerlockerror, false );
+  instructions.addEventListener( 'click', function ( event ) {
+    instructions.style.display = 'none';
+    // Ask the browser to lock the pointer
+    element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+    if ( /Firefox/i.test( navigator.userAgent ) ) {
+      var fullscreenchange = function ( event ) {
+        if ( document.fullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element ) {
+          document.removeEventListener( 'fullscreenchange', fullscreenchange );
+          document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
+          element.requestPointerLock();
+        }
+      };
+      document.addEventListener( 'fullscreenchange', fullscreenchange, false );
+      document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
+      element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
+      element.requestFullscreen();
+    } else {
+      element.requestPointerLock();
+    }
+  }, false );
+} else {
+  instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
+}
+
+var onKeyDown = function ( event ) {
+  switch ( event.keyCode ) {
+    case 38: // up
+    case 87: // w
+      moveForward = true;
+      break;
+    case 37: // left
+    case 65: // a
+      moveLeft = true; break;
+    case 40: // down
+    case 83: // s
+      moveBackward = true;
+      break;
+    case 39: // right
+    case 68: // d
+      moveRight = true;
+      break;
+    case 32: // space
+      if ( canJump === true ) velocity.y += 350;
+      canJump = false;
+      break;
+  }
+};
+var onKeyUp = function ( event ) {
+  switch( event.keyCode ) {
+    case 38: // up
+    case 87: // w
+      moveForward = false;
+      break;
+    case 37: // left
+    case 65: // a
+      moveLeft = false;
+      break;
+    case 40: // down
+    case 83: // s
+      moveBackward = false;
+      break;
+    case 39: // right
+    case 68: // d
+      moveRight = false;
+      break;
+  }
+};
+document.addEventListener( 'keydown', onKeyDown, false );
+document.addEventListener( 'keyup', onKeyUp, false );
