a11016eccbebaaeafbc958eff922300916243c01
Clark Van Oyen <clark@countable.ca>
2017-03-20 11:52:23 -0700

    Switch to OSM

--- minimap.js
+++ minimap.js
@@ -1,11 +1,8 @@
 var minimap = L.map('mapid').setView([player.lat, player.lng], 17);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 18,
-  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-  id: 'mapbox.streets'
+  attribution: ' '
 }).addTo(minimap);
 
 var arrow_icon = L.divIcon({
